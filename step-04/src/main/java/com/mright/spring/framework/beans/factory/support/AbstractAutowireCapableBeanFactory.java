a7dec55f447e3af1c006d36b6b164499a02858a2
zhaochuanzhen <zhaochuanzhen@jd.com>
2021-11-24 14:37:14 +0800

    step04

--- step-04/src/main/java/com/mright/spring/framework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ step-04/src/main/java/com/mright/spring/framework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,7 +1,11 @@
 package com.mright.spring.framework.beans.factory.support;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.mright.spring.framework.beans.BeansException;
+import com.mright.spring.framework.beans.PropertyValue;
+import com.mright.spring.framework.beans.PropertyValues;
 import com.mright.spring.framework.beans.factory.config.BeanDefinition;
+import com.mright.spring.framework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Constructor;
 import java.text.MessageFormat;
@@ -24,9 +28,30 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
     @Override
     protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) {
         try {
-            return createBeanInstance(beanDefinition, beanName, args);
+            Object beanInstance = createBeanInstance(beanDefinition, beanName, args);
+            // 属性填充
+            applyPropertyValues(beanName, beanInstance, beanDefinition);
+            addSingleton(beanName, beanInstance);
+            return beanInstance;
         } catch (Exception e) {
             throw new BeansException(MessageFormat.format("实例化Bean失败. beanName:{0}", beanName));
+        }
+    }
+
+    protected void applyPropertyValues(String beanName, Object beanInstance, BeanDefinition beanDefinition){
+        PropertyValues propertyValues = beanDefinition.getPropertyValues();
+        if (propertyValues == null) {
+            return;
+        }
+        for (PropertyValue propertyValue : propertyValues.getPropertyValues()) {
+            String name = propertyValue.getName();
+            Object value = propertyValue.getValue();
+            if (value instanceof BeanReference) {
+                // 如果value是一个bean的引用，此处需要递归处理，获取所有bean
+                value = getBean(((BeanReference) value).getBeanName());
+            }
+            // 属性直接填充
+            BeanUtil.setProperty(beanInstance, name, value);
         }
     }
 
--- step-04/src/test/java/com/mright/spring/framework/test/Test04.java
+++ step-04/src/test/java/com/mright/spring/framework/test/Test04.java
@@ -1,22 +1,47 @@
 package com.mright.spring.framework.test;
 
+import com.mright.spring.framework.bean.UserDao;
 import com.mright.spring.framework.bean.UserService;
+import com.mright.spring.framework.beans.PropertyValue;
+import com.mright.spring.framework.beans.PropertyValues;
 import com.mright.spring.framework.beans.factory.config.BeanDefinition;
+import com.mright.spring.framework.beans.factory.config.BeanReference;
 import com.mright.spring.framework.beans.factory.support.DefaultListableBeanFactory;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test04 {
 
     @Test
-    public void test() {
+    public void test1() {
         BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
 
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
-        System.out.println(beanFactory.getBean("userService"));
-        System.out.println(beanFactory.getBean("userService"));
-        ((UserService)beanFactory.getBean("userService", "zhaochuanzhen")).sayHello();
+        UserService userService1 = (UserService) beanFactory.getBean("userService", "zhaochuanzhen");
+        userService1.sayHello();
+
+        UserService userService2 = (UserService) beanFactory.getBean("userService", "chenduxiu");
+        userService2.sayHello();
+    }
+
+    @Test
+    public void test2() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        beanFactory.registerBeanDefinition("userDao", new BeanDefinition(UserDao.class));
+
+        PropertyValues propertyValues = new PropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("id", "10001"));
+        propertyValues.addPropertyValue(new PropertyValue("userDao", new BeanReference("userDao")));
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class, propertyValues);
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+        UserService userService = (UserService) beanFactory.getBean("userService", "zhaochuanzhen");
+
+        userService.queryUserInfo();
     }
 }
