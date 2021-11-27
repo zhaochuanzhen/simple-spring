d532d9920aa45f79c9907ede24cd8152b6d15b21
zhaochuanzhen <zhaochuanzhen@jd.com>
2021-11-27 23:59:57 +0800

    step09

--- step-09/src/test/java/com/mright/spring/framework/test/Test07.java
+++ step-09/src/test/java/com/mright/spring/framework/test/Test07.java
@@ -44,4 +44,15 @@ public class Test07 {
         System.out.println(userService01 + " 十六进制哈希：" + Integer.toHexString(userService01.hashCode()));
 //        System.out.println(ClassLayout.parseInstance(userService01).toPrintable());
     }
+
+    @Test
+    public void test_factory_bean() {
+        // 1.初始化 BeanFactory
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        // 2. 调用代理方法
+        IndexService userService = applicationContext.getBean("indexService", IndexService.class);
+        System.out.println("测试结果：" + userService.queryUserInfo());
+    }
 }
