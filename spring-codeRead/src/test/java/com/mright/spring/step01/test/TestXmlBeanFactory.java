df93cf5bc91bc94bc923c4f4fe1a33ddc204c4af
zhaochuanzhen <zhaochuanzhen@jd.com>
2021-12-19 19:02:05 +0800

    code-read

--- spring-codeRead/src/test/java/com/mright/spring/step01/test/TestXmlBeanFactory.java
+++ spring-codeRead/src/test/java/com/mright/spring/step01/test/TestXmlBeanFactory.java
@@ -1,18 +1,21 @@
 package com.mright.spring.step01.test;
 
+import com.mright.spring.step01.bean.UserService;
 import org.junit.Test;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class TestXmlBeanFactory {
 
     @Test
     public void test01() {
-        ClassPathResource resource = new ClassPathResource("spring.xml");
-        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
-        final XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
-        final int i = reader.loadBeanDefinitions(resource);
-        System.out.println(i);
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("classpath:spring.xml");
+//        ClassPathResource resource = new ClassPathResource("spring.xml");
+//        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+//        final XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+//        final int i = reader.loadBeanDefinitions(resource);
+//        System.out.println(i);
+
+        final UserService userService = context.getBean(UserService.class);
+        userService.sayHello();
     }
 }
